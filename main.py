68098c5577e2295c48de93cde520fa8838578feb
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-05-13 20:09:16 -0500

    CLASE 13/05/25

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+usuarios = []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def crud():
+    nombre = request.form.get('nombre')
+    email = request.form.get('email')
+    usuarios.append({'username': nombre, 'email': email})
+
+    return render_template('registro.html', usuarios=usuarios)
+
+
+if __name__ == '__name__':
+    app.run(debug=True)
