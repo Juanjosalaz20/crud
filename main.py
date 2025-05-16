a6949fd57cb8f713685172a2c295d46c39d609d9
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-05-15 20:06:59 -0500

    CLASE 15/05/25

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 usuarios = []
 
+id_contador = 1
 
 @app.route('/', methods=['GET', 'POST'])
 def crud():
-    nombre = request.form.get('nombre')
-    email = request.form.get('email')
-    usuarios.append({'username': nombre, 'email': email})
+    global id_contador
+
+    if request.method == 'POST': # Para evitar que al recargar la pagina se agrguem usuarios vacio por el get (si accedemos a la ruta con datos en el formulario)
+        nombre = request.form.get('nombre')
+        email = request.form.get('email')
+        usuarios.append({'id': id_contador, 'username': nombre, 'email': email})
+        id_contador += 1
+        return redirect(url_for('crud'))
+
+    id_eliminar = request.args.get('borrar')
+    if id_eliminar:
+        #TODO: Eliminar el usuario con el id del parametro de la lista
+        for item in usuarios:
+            if str(item['id'])==id_eliminar:
+                usuarios.remove(item)
+                break
+        return redirect(url_for('crud')) # Llamar el nombre de la función
+
 
     return render_template('registro.html', usuarios=usuarios)
 
